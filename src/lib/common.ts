53674ad1b70ae6a54979bf97343c28cafb35ef42
Moriz von Langa <morizvlanga@gmail.com>
2025-06-19 22:41:46 +0200

    Applied coderabbit feedback

--- src/lib/common.ts
+++
@@ -1,9 +0,0 @@
-export const slugify = (str: string) => {
-  str = str.replace(/^\s+|\s+$/g, '');
-  str = str.toLowerCase();
-  str = str.replace(/[^a-z0-9 -]/g, '')
-    .replace(/\s+/g, '-')
-    .replace(/-+/g, '-');
-
-  return str;
-}
